9cceb4a71fcd461abd5ab7b2f1acc391be95202a
Santiago <santitati1204@gmail.com>
2025-08-01 02:49:03 -0300

    Primer commit del proyecto

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgfyy7LPOl0qKmY21BjkrQRV-rPxLeyvk",
+  authDomain: "mayra-ca7e3.firebaseapp.com",
+  projectId: "mayra-ca7e3",
+  storageBucket: "mayra-ca7e3.firebasestorage.app",
+  messagingSenderId: "1044029271534",
+  appId: "1:1044029271534:web:98e5bc6de1ab76023732ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
